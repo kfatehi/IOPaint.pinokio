99a40e85a3969ca128621e56092b5e6da7f1c989
Keyvan Fatehi <keyvanfatehi@gmail.com>
2023-12-22 19:17:25 -0800

    init

---
+++ pinokio.js
@@ -0,0 +1,28 @@
+const os = require('os')
+module.exports = {
+  title: "LamaCleaner",
+  icon: "icon.png",
+  description: "A free and open-source inpainting tool powered by SOTA AI model https://github.com/Sanster/lama-cleaner",
+  menu: async (kernel) => {
+    let installed = await kernel.exists(__dirname, "app", "env")
+    if (installed) {
+      let running = await kernel.running(__dirname, "start.json")
+      if (running) {
+        return [
+          { icon: "fa-solid fa-spin fa-circle-notch", text: "Running" },
+          { icon: "fa-solid fa-terminal", text: "Terminal", href: "start.json" }
+        ]
+      } else {
+        return [
+          { icon: "fa-solid fa-power-off", text: "start", href: "start.json", params: { fullscreen: true, run: true } },
+        ]
+      }
+    } else {
+      if (os.platform() === 'win32') {
+        return [{ icon: "fa-solid fa-plug", text: "Install", href: "install_win.json", params: { run: true, fullscreen: true } }]
+      } else {
+        return [{ icon: "fa-solid fa-plug", text: "Install", href: "install.json", params: { run: true, fullscreen: true } }]
+      }
+    }
+  }
+}
